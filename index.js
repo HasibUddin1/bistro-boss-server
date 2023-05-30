584e62d953a66b30bba6d6e3b24a79b2899b7d8f
IconicLuck <uhasib959@gmail.com>
2023-05-30 15:34:56 +0600

    implemented jwt verification

--- index.js
+++ index.js
@@ -6,8 +6,27 @@ const app = express();
 require('dotenv').config()
 const port = process.env.PORT || 5000
 
+// middleware
 app.use(cors())
 app.use(express.json())
+
+const verifyJWT = (req, res , next) => {
+  const authorization = req.headers.authorization
+  if(!authorization){
+    return res.status(401).send({error: true, message: 'unauthorized access'})
+  }
+
+  const token = authorization.split(' ')[1]
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if(err){
+      return res.status(401).send({error: true, message: 'unauthorized access'})
+    }
+
+    req.decoded = decoded
+    next();
+  })
+}
 
 
 
@@ -90,10 +109,15 @@ async function run() {
 
     // cart collection related apis
 
-    app.get('/cart', async (req, res) => {
+    app.get('/cart', verifyJWT, async (req, res) => {
       const email = req.query.email
       if (!email) {
         res.send([])
+      }
+
+      const decodedEmail = req.decoded.email
+      if(email !== decodedEmail){
+        return res.status(403).send({error: true, message: 'forbidden access'})
       }
 
       const query = { email: email }
