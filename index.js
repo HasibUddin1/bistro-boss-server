9b399b03d166894394a54d81ea4b4b90b9abe908
IconicLuck <uhasib959@gmail.com>
2023-06-02 16:24:52 +0600

    created a payment intent for payment

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors')
 const jwt = require('jsonwebtoken');
+const stripe = require('stripe')(process.env.PAYMENT_SECRET_KEY)
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 require('dotenv').config()
@@ -183,6 +184,22 @@ async function run() {
       const query = { _id: new ObjectId(id) }
       const result = await cartCollection.deleteOne(query)
       res.send(result)
+    })
+
+    // create a new payment intent
+    app.post('/create-payment-intent', async (req, res) => {
+      const {price} = req.body;
+      const amount = price*100
+
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: 'usd',
+        payment_method_types: ['card']
+      })
+
+      res.send({
+        clientSecret: paymentIntent.client_secret
+      })
     })
 
 
