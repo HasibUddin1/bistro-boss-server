b4aba9c01160db6a113d9833999f784970b94e93
IconicLuck <uhasib959@gmail.com>
2023-06-04 01:19:30 +0600

    created admin stats fetch link for admin home

--- index.js
+++ index.js
@@ -217,6 +217,22 @@ async function run() {
       res.send({ insertResult, deleteResult })
     })
 
+    app.get('/admin-stats', async (req, res) => {
+      const users = await usersCollection.estimatedDocumentCount()
+      const products = await menuCollection.estimatedDocumentCount()
+      const orders = await paymentCollection.estimatedDocumentCount()
+
+      const payments = await paymentCollection.find().toArray()
+      const revenue = payments.reduce((sum, payment) => sum + payment.price, 0)
+
+      res.send({
+        users,
+        products,
+        orders,
+        revenue
+      })
+    })
+
 
 
     // Send a ping to confirm a successful connection
